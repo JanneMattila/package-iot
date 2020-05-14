7041a3bf2f94d38a571c4f4c49c7b8a289b3234c
Janne Mattila <janne.mattila@hotmail.fi>
2020-05-14 09:27:41 +0300

    Add placeholder project

---
+++ src/PackageDevice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PackageDevice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
